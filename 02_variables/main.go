bae2177bcef733f031e57619d91b7d44c33164f5
awsdevopsyasila <awsdevopsyasila@gmail.com>
2021-08-24 17:30:03 +0700

    First commit

---
+++ 02_variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("variable declaration")
+	var name string = "Yasila"
+	var isMale = true
+
+	fmt.Println(name, isMale)
+	fmt.Printf("%T\n", isMale)
+
+}
---
+++ 03_packages/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/awsdevopsyasila/go_crash_course/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println("packages example")
+	fmt.Println(strutil.Reverse("olleh"))
+}
